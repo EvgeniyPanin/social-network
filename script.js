ca1dceb68d2109ec2e1504debdc6911de0d9e52b
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-05-06 13:23:13 +0400

    validation buttonSubmit enabled/disabled

--- script.js
+++ script.js
@@ -110,10 +110,14 @@ function formAddCard(event) {
   event.preventDefault();
   const form = event.currentTarget;
   const {name : formName, link : formLink} = form.elements;
+  const buttonForm = form.querySelector('.popup__button');
   
   addCard(formName.value, formLink.value);
   
   form.reset();
+  buttonForm.setAttribute('disabled', true);
+  buttonForm.classList.add('popup__button_disabled');
+
   popupAddPlace.classList.remove('popup_is-opened');
 }
 
@@ -133,5 +137,48 @@ function editUserData(event) {
 
 formEditProfile.addEventListener('submit', editUserData);
 
+function submitButtonEnabled(event) {
+  const form = event.currentTarget;
+  const submitButton = form.querySelector('.popup__button');
+
+  switch (form) {
+    case formAddPlace:
+      const {name : nameMesto, link} = form.elements;
+
+      if ((nameMesto.value.length === 0) || (link.value.length === 0)) {
+        submitButton.setAttribute('disabled', true);
+        submitButton.classList.add('popup__button_disabled');
+      } else {
+        submitButton.removeAttribute('disabled');
+        submitButton.classList.remove('popup__button_disabled');
+      }
+      
+      break;
+
+    case formEditProfile:
+      const {name : nameUser, about} = form.elements;
+      console.log(nameUser.value, about.value);
+
+      break;
+  }
+  
+}
+
+formAddPlace.addEventListener('input', submitButtonEnabled);
+formEditProfile.addEventListener('input', submitButtonEnabled);
+
+
+/* 
+form.addEventListener('input', function(event) {
+  const form = document.forms.add;
+  if ((artist.value.length === 0) || (song.value.length === 0)) {
+    addButton.setAttribute('disabled', true);
+    addButton.classList.add('input__btn_disabled');
+  } else {
+    addButton.removeAttribute('disabled');
+    addButton.classList.remove('input__btn_disabled');
+  }
+})
+ */
 
 
