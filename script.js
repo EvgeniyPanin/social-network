334646837e3478c040940ef545a64d90cba810fe
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-05-11 11:25:22 +0400

    Исправил замечания после ревью 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Можно лучше
-// Используйте строгий режим -- позволит найти все невено объявленные переменные
+// Используйте строгий режим -- позволит найти все невено объявленные переменные +
 // https://learn.javascript.ru/strict-mode
 "use strict";
 
@@ -13,7 +13,11 @@ const popupCloseButtons = document.querySelectorAll('.popup__close');
 const userName = document.querySelector('.user-info__name');
 const userJob = document.querySelector('.user-info__job');
 const formAddPlace = document.forms.new;
+const inputsAddCard = formAddPlace.querySelectorAll('input');
+const buttonSubmitAddForm = formAddPlace.querySelector('button');
 const formEditProfile = document.forms.edit;
+const inputsEditCard = formEditProfile.querySelectorAll('input');
+const buttonSubmitEditForm = formEditProfile.querySelector('button');
 const template = document.querySelector('#card-template').content.querySelector('.place-card');
 const errorMessage = {
   textErrorLength: 'Должно быть от 2 до 30 символов',
@@ -23,22 +27,22 @@ const errorMessage = {
 
 
 // создает пустую карточку места
-function createCard() {                                      
+function createCard() {
   const newCard = template.cloneNode(true);
   return newCard;
 }
 
-
 // добавляет данные в карточку
 function addDataCard(newCard, cardName, imageLink) {
-  newCard.querySelector('.place-card__image').style = `background-image: url(${imageLink})`;
+  const image = newCard.querySelector('.place-card__image');
+  image.style = `background-image: url(${imageLink})`;
+  image.dataset.src = imageLink;
   newCard.querySelector('.place-card__name').textContent = cardName;
   return newCard;
 }
 
-
 // добавляет готовую карточку с данными в разметку
-function addCard(name, link) {                                
+function addCard(name, link) {
   const cardItem = createCard();
   const doneCard = addDataCard(cardItem, name, link);
   cardsContainer.appendChild(doneCard);
@@ -48,6 +52,117 @@ function addCard(name, link) {
 initialCards.forEach(place => {
   addCard(place.name, place.link)
 });
+
+// Функция валидирующая поле, и устанавливающая кастомную ошибку валидации
+function isValidity(field) {
+  field.setCustomValidity("");
+
+  if (field.validity.valueMissing) {
+    field.setCustomValidity(errorMessage.textErrorEmptyString);
+    return false
+  }
+
+  if (field.validity.tooShort || field.validity.tooLong) {
+    field.setCustomValidity(errorMessage.textErrorLength);
+    return false
+  }
+
+  if (field.validity.typeMismatch && field.type === 'url') {
+    field.setCustomValidity(errorMessage.textErrorURL);
+    return false
+  }
+
+  return field.checkValidity();
+}
+
+// Добавляет текст ошибки под полем, в котором происходит событие ввода, если оно не валидно
+function isFieldValidity(field) {
+  const valid = isValidity(field);
+  // Это лишнее условие    ------   Условие позволяет выставлять ошибку валидации только на поле в котором произошло событие
+  // 1. Вы event не пердаете, а получаете глобально
+  // 2. Событие и так на поле происходит, вы же target передаете в метод, не currentTarget
+  if (field === event.target) {
+    const errorElem = field.parentNode.querySelector(`#${field.id}-error`);
+    errorElem.textContent = field.validationMessage;
+  }
+
+  return valid;
+}
+
+// Проверяет все ли переданные инпуты валидны
+function checkFormValidity(inputs) {
+  let valid = true;
+
+  inputs.forEach(input => {
+    if (!isFieldValidity(input)) valid = false;
+  })
+
+  return valid;
+}
+
+// выставляет кнопку в Enbld/Dsbld в зависимости от того true или false принятое значение state
+function toggleButton(submitButton, state) {
+  if (state) {
+    submitButton.removeAttribute('disabled');
+    submitButton.classList.remove('popup__button_disabled');
+  } else {
+    submitButton.setAttribute('disabled', true);
+    submitButton.classList.add('popup__button_disabled');
+  }
+}
+
+// выводит на страницу переданный попап
+function showPopup(popup) {
+  popup.classList.add('popup_is-opened');
+}
+
+// коллбек, передаваемый слушателям событий клика по элементам, открывающим попапы
+function openedPopup(event) {
+  const clickElem = event.currentTarget;
+  let valid;
+
+  if (clickElem === addButton) {
+    // Посоветую так -- лучше если кнопка будет при открытии всегда заблокирована +
+    // При таком подходе исключено случайное нажатие и повторная отправка неизмененных данных на сервер (лишняя операция)
+    // Надо исправить +
+    // Вы передаете все элементы, по которым потом прогоняете валидацию
+    // А надо только инпуты
+    // Соответственно, логично заранее (не в этом методе) выбрать инпуты обеих форм в разные массивы,
+    // как и кнопки сабмита, и тут уже просто скармливать их методу.
+    // Исправьте для обоих форм
+    // Возможно тут вообще не потребуются исправления а метод будет изменен вами,
+    // прочитайте все ревью до конца, особенно комментарий в методе установки слушателей валидации
+    valid = checkFormValidity(inputsAddCard);
+    toggleButton(buttonSubmitAddForm, valid);
+    showPopup(popupAddPlace);
+  }
+
+  if (clickElem === editButton) {
+    const { name, about: Job } = formEditProfile.elements;
+
+    name.value = userName.textContent;
+    Job.value = userJob.textContent;
+
+    valid = checkFormValidity(inputsEditCard);
+    toggleButton(buttonSubmitEditForm, valid);
+    showPopup(popupEditProfile);
+  }
+
+  if (event.target.classList.contains('place-card__image')) {
+    const imageItem = event.target.closest('.place-card__image');
+    const src = imageItem.dataset.src;
+    // Надо исправить +
+    // Не надо вырезать URL картинки сложным парсингом
+    // Добавьте data-атрибут, сылку я давал
+    // Завтра дизайнер кавычки лишние поставит или пробел не там и сломается ваш парсинг
+    
+    const image = popupImage.querySelector('.popup__image');
+
+    image.setAttribute('src', src);
+
+    showPopup(popupImage);
+  }
+}
 
 // Управляет действиями с контентом карточки в зависимости от того, в каком ее месте кликнули
 function contentManagement(event) {
@@ -70,54 +185,6 @@ function contentManagement(event) {
 // слушаем событие клика по контейнеру с карточками
 cardsContainer.addEventListener('click', contentManagement);
 
-// выводит на страницу переданный попап
-function showPopup(popup) {
-  popup.classList.add('popup_is-opened');
-}
-
-
-// проверяет все ли поля формы валидны, и в зависимости от результата выставлет состояние кнопки
-function toggleButton(InputsArray, submitButton) {
-  if (InputsArray.every(isValidity)) {
-    setSubmitButton(submitButton, true);
-  } else {
-    setSubmitButton(submitButton, false);
-  }
-}
-
-// коллбек, передаваемый слушателям событий клика по элементам, открывающим попапы
-function openedPopup(event) {
-  const clickElem = event.currentTarget;
-
-  if (clickElem === addButton) {
-    const submitButton = formAddPlace.querySelector('.popup__button');
-
-    // переключаем состояние кнопки, в зависимости от того валидна ли форма в момент открытия попапа
-    toggleButton([...formAddPlace], submitButton); 
-    showPopup(popupAddPlace);
-  }
-
-  if (clickElem === editButton) {
-    const submitButton = formEditProfile.querySelector('.popup__button');
-    const { name, about: Job } = formEditProfile.elements;
-    
-    name.value = userName.textContent;
-    Job.value = userJob.textContent;
-
-    toggleButton([...formEditProfile], submitButton);
-    showPopup(popupEditProfile);
-  }
-
-  if (event.target.classList.contains('place-card__image')) {
-    const imageItem = event.target.closest('.place-card__image');
-    const src = imageItem.style.backgroundImage.replace('url(','').replace(')','').replace(/\"/gi, "");
-    const image = popupImage.querySelector('.popup__image');
-
-    image.setAttribute('src', src);
-
-    showPopup(popupImage);
-  }
-}
 
 addButton.addEventListener('click', openedPopup);
 editButton.addEventListener('click', openedPopup);
@@ -140,11 +207,10 @@ function closedPopup(event) {
   if (popup.id === 'image-popup') {
     popup.classList.remove('popup_is-opened');
     return;
-  } else {
-    resetErrorsForm(form);
-    popup.classList.remove('popup_is-opened');
+    // После return не нужен else +
   }
-  
+  resetErrorsForm(form);
+  popup.classList.remove('popup_is-opened');
 }
 
 // Проходим по всем закрывающим кнопкам и вешаем на них слушатель клика
@@ -179,68 +245,38 @@ function editUserData(event) {
 
 formEditProfile.addEventListener('submit', editUserData);
 
-// Функция валидирующая поле, и устанавливающая кастомную ошибку валидации
-function isValidity(field) {
-  field.setCustomValidity("");
-
-  if (field.validity.valueMissing) {
-    field.setCustomValidity(errorMessage.textErrorEmptyString);
-    return false
-  }  
-
-  if (field.validity.tooShort || field.validity.tooLong) {
-    field.setCustomValidity(errorMessage.textErrorLength);
-    return false
-  }
-
-  if (field.validity.typeMismatch && field.type === 'url') {
-    field.setCustomValidity(errorMessage.textErrorURL);
-    return false
-  } 
-
-  return field.checkValidity();
-}
-
-
-// Добавляет текст ошибки под полем, в котором происходит событие ввода, если оно не валидно
-function isFieldValidity(field) {
-  const errorElem = field.parentNode.querySelector(`#${field.id}-error`);
-  const valid = isValidity(field)
-  if (field === event.target) {
-    errorElem.textContent = field.validationMessage;
-  }
-  
-  return valid;
-}
-
-// выставляет кнопку в Enbld/Dsbld в зависимости от того true или false принятое значение state
-function setSubmitButton(submitButton, state) {
-  if (state) {
-    submitButton.removeAttribute('disabled');
-    submitButton.classList.remove('popup__button_disabled');
-  } else {
-    submitButton.setAttribute('disabled', true);
-    submitButton.classList.add('popup__button_disabled');
-  }
-}
-
 // Коллбэк слушателя события ввода на формах,
-function handlerInputForm(event) {
-  const submitButton = event.currentTarget.querySelector('.button');
-  const inputs = Array.from(event.currentTarget.elements);
+function handlerInputForm(event, inputs, submitButton) {
 
-  // выводит сообщения под полем в котором идет ввод
-  isFieldValidity(event.target);
+  // выводит сообщения под полем в котором идет ввод и записывает в valid валидна ли форма
+  const valid = checkFormValidity(inputs);
 
-  //переключает кнопку Submit
-  toggleButton(inputs, submitButton);
-} 
+  // У функции должно быть одно назначение. toggleButton должна получить true/false как флаг +
+  // включения-отключения и элемент кнопки
+  // Проверку валидности из toggleButton лучше сюда перенести, это логичнее, тем самым +
+  // вы сможете включать-выключать кнопку когда захотите не перебирая инпуты и прочее и не делая
+  // массу ненужных запросов в DOM
+
+  if (valid) {
+    toggleButton(submitButton, true);
+  } else {
+    toggleButton(submitButton, false);
+  }
+}
 
 // проставляет прослушивание событий на формах
 function setEventListeners(popup) {
-  const form = popup.querySelector('.popup__form')
+  const form = popup.querySelector('.popup__form');
+  const inputs = Array.from(popup.querySelectorAll('input'));
+  const submitButton = popup.querySelector('button');
 
-  form.addEventListener('input', handlerInputForm);
+  // Надо исправить
+  // Здесь получите массив инпутов формы, кнопку сабмита, +
+  // можно даже создать объект с элементами ошибок, где ключом будет id элемента
+  // И все эти данне передайте в handlerInputForm + 
+  // Тем самым вам не придется многократно получать эти элементы в последствии и нагружать DOM
+
+  form.addEventListener('input', () => handlerInputForm(event, inputs, submitButton));
 }
 
 setEventListeners(popupEditProfile);
@@ -249,16 +285,14 @@ setEventListeners(popupAddPlace);
 
 // Здравствуйте.
 
-// Работа отклонена от проверки -- окно попапа с увеличенным изображением не закрывается, а значит не работает обязательный функционал +
-// Также обращу ваше внимание, что метод поиска URL картинки у вас неоптимальный и ненужный +
-// При создании карточки добавьте атрибут с url элементу карточки, а когда будет необходимо
+// Нужен рефакторинг, рекомендации в коде. Стало лучше чем было в прошлый раз, но можно сделать еще проще и чище.
+
+// При создании карточки добавьте атрибут с url элементу карточки, а когда будет необходимо +
 // -- прочитаете его. Как это сделать https://developer.mozilla.org/ru/docs/Web/HTML/Global_attributes/data-*
-// Так как реализовано у вас -- принято не будет.
-
-// За исключением исходного массива код следует объединить в один файл. +
-//  if ((input1.value.length === 0) || (input2.value.length === 0)) -- не валидация, а имена с числительными -- нельзя +
+// Надо исправить
 
 
-// Вы явно читали этот материал: https://developer.mozilla.org/ru/docs/Learn/HTML/Forms/%D0%92%D0%B0%D0%BB%D0%B8%D0%B4%D0%B0%D1%86%D0%B8%D1%8F_%D1%84%D0%BE%D1%80%D0%BC%D1%8B
 
-// У вас очень сложно все и много лишних условий, постарайтесь проще подойти и не усложнять
+
+
+
